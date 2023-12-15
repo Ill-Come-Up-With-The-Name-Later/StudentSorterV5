8f3e3ecd0a6f5055004511bf9d5189244bfbdb8d
MagiD <magidonovan@gmail.com>
2023-12-15 13:34:14 -0500

    Added comment to Sort constructor

--- StudentSorter/Sort.cs
+++ StudentSorter/Sort.cs
@@ -8,6 +8,20 @@ namespace StudentSorter
         public string GroupFile { get; set; }
         public string IllegalPairsFile { get; set; }
 
+        /// <summary>
+        /// Creates a Sorter Configuration
+        /// </summary>
+        /// <param name="studentFile">
+        /// The path of a file containing students' names
+        /// </param>
+        /// <param name="groupFile">
+        /// The path of a file containing information
+        /// about groups
+        /// </param>
+        /// <param name="illegalPairFile">
+        /// The path of a file containing information about
+        /// illegal pairs
+        /// </param>
         [JsonConstructor]
         public Sort(string studentFile, string groupFile, string illegalPairFile) 
         {
