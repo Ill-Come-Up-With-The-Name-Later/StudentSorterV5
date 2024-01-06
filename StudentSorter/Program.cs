9f13dd90680e59883d701daeb4281c13a2654c89
MagiD <magidonovan@gmail.com>
2024-01-06 07:34:53 -0500

    Update version number

--- StudentSorter/Program.cs
+++ StudentSorter/Program.cs
@@ -2,7 +2,7 @@ namespace StudentSorter
 {
     static class Program
     {
-        public const string Version = "0.4.7";
+        public const string Version = "0.4.7.1";
 
         /// <summary>
         ///  The main entry point for the application.
