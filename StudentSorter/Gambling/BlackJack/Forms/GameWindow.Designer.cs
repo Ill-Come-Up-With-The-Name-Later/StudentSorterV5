9e20ced7f42151fc3927fabae26ca8028688badd
MagiD <magidonovan@gmail.com>
2024-01-23 19:39:18 -0500

    Added window to display the game

---
+++ StudentSorter/Gambling/BlackJack/Forms/GameWindow.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace StudentSorter.Gambling.BlackJack.Forms
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // GameWindow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(863, 499);
+            MaximizeBox = false;
+            Name = "GameWindow";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Game Window";
+            Load += GameWindow_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ StudentSorter/Gambling/BlackJack/Forms/GameWindow.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentSorter.Gambling.BlackJack.Forms
+{
+    public partial class GameWindow : Form
+    {
+        public GameManager Manager;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+
+            Player p1 = new();
+            Player p2 = new("Dealer");
+            Manager = new(p1, p2);
+        }
+
+        /// <summary>
+        /// Set up blackjack game
+        /// </summary>
+        private void GameWindow_Load(object sender, EventArgs e)
+        {
+            Manager.SetupGame();
+        }
+    }
+}
