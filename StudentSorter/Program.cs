f24d7fbbad145f787aa544acd8ffecf13224fd2b
MagiD <magidonovan@gmail.com>
2024-02-06 11:57:03 -0500

    version number: 0.10 -> 0.10.1

--- StudentSorter/Program.cs
+++ StudentSorter/Program.cs
@@ -4,7 +4,7 @@ namespace StudentSorter
 {
     static class Program
     {
-        public const string Version = "0.10";
+        public const string Version = "0.10.1";
 
         public static DebugWindow Debugger = new();
 
